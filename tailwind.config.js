0da4190a907428c4b4f831af29ce2278aa074338
Diego <diegogkx@gmail.com>
2022-03-10 13:34:24 -0400

    fixed color theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,27 +2,27 @@ module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
     fill: (theme) => ({
-      red: theme('colors.red.primary')
+      red: theme("colors.red.primary"),
     }),
-    colors:{
-      white: '#ffffff',
+    colors: {
+      white: "#ffffff",
       blue: {
-        medium: '#005c98'
+        medium: "#005c98",
       },
       black: {
-        light: '#005c98',
-        faded: '#00000059'
+        light: "#262626",
+        faded: "#00000059",
       },
       gray: {
-        base: '#616161',
-        background: '#fafafa',
-        primary: "#dbdbdb"
+        base: "#616161",
+        background: "#fafafa",
+        primary: "#dbdbdb",
       },
       red: {
-        primary: '#ed4956',
-      }
+        primary: "#ed4956",
+      },
     },
     extend: {},
   },
   plugins: [],
-}
+};
