fca3ae662f1afa51aad3ea385d8d512930c2b5ed
Diego <diegogkx@gmail.com>
2022-03-23 00:24:02 -0400

    photo grid now working

--- src/components/userprofile/photos.js
+++ src/components/userprofile/photos.js
@@ -3,7 +3,27 @@ import PropTypes from "prop-types";
 import Skeleton from "react-loading-skeleton";
 
 export default function Photos({ photos }) {
-  return null;
+  console.log(photos);
+  return (
+    <div className="h-16 border-t border-gray-primary mt-12 pt-4">
+      <div className="grid grid-cols-3 gap-8 mt-4 mb-12">
+        {!photos ? (
+          <>
+            <Skeleton count={12} width={320} height={400} />
+          </>
+        ) : photos.length > 0 ? (
+          photos.map((photo) => (
+            <div key={photo.docId} className="relative group">
+              <img src={photo.imageSrc} alt={photo.caption} />
+              <div className="absolute bottom-0 left-0 bg-gray-200 z-10 w-full justify-evenly items-center h-full bg-black-faded group-hover:flex">
+                <p className="flex items-center text-white font-bold">hello</p>
+              </div>
+            </div>
+          ))
+        ) : null}
+      </div>
+    </div>
+  );
 }
 
 Photos.propTypes = {
