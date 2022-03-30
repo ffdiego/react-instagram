71c7ee66ce772c4fb5ef022331a1d3c370612f76
Diego <diegogkx@gmail.com>
2022-03-29 22:34:09 -0400

    variable size avatar component

--- src/components/userprofile/overlay/avatar.js
+++ src/components/userprofile/overlay/avatar.js
@@ -1,10 +1,10 @@
 import { Link } from "react-router-dom";
 
-export default function Avatar({ username }) {
+export default function Avatar({ username, size }) {
   return (
     <Link to={`/${username}`}>
       <img
-        className="w-8 rounded-full mr-2"
+        className={`w-${size ? size : "8"} rounded-full mr-2`}
         alt={`${username} avatar`}
         src={`/images/avatars/${username}.jpg`}
       />
