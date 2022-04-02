156d972cf7d5c3c50ff5be3a653709bdbf920ed7
Diego <diegogkx@gmail.com>
2022-04-02 10:18:53 -0400

    fixed footer not linking to profile

--- src/components/post/footer.js
+++ src/components/post/footer.js
@@ -1,9 +1,12 @@
 import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
 
 export default function Footer({ author, caption }) {
   return (
     <div className="p-4 pt-2 pb-0">
-      <span className="mr-1 font-bold">{author}</span>
+      <span className="mr-1 font-bold">
+        <Link to={`/${author}`}>{author}</Link>
+      </span>
       <span>{caption}</span>
     </div>
   );
