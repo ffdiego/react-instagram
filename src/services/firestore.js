35ef9394198a8b14179573e44c1a3512cc55b8c2
Diego <diegogkx@gmail.com>
2022-04-06 13:43:00 -0400

    getFile function added

--- src/services/firestore.js
+++ src/services/firestore.js
@@ -1,9 +1,16 @@
 import { storage } from "../lib/firebase";
-import { ref, uploadBytesResumable } from "firebase/storage";
+import { ref, uploadBytesResumable, getDownloadURL } from "firebase/storage";
 
 export function uploadFile(file) {
-  const fileRef = ref(storage, file.name);
+  const fileRef = ref(storage, "avatars/" + file.name);
   const uploadTask = uploadBytesResumable(fileRef, file);
 
   return uploadTask;
+}
+
+export function getFile(setImg) {
+  const gsReference = ref(storage, "avatars/mech.png");
+  getDownloadURL(gsReference).then((url) => {
+    setImg(url);
+  });
 }
