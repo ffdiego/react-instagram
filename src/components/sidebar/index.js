219ec595a1c75e9ee88c43a2719fa1ee4d75af1c
Diego <diegogkx@gmail.com>
2022-04-11 11:08:29 -0400

    added link to my github page

--- src/components/sidebar/index.js
+++ src/components/sidebar/index.js
@@ -9,6 +9,10 @@ export default function Sidebar() {
     <div className="mt-20 p-4">
       <User username={username} fullname={fullname} />
       <Suggestions username={username} following={following} />
+      <p className="my-10 font-thin text-sm text-gray-base text-right">
+        by Diego Fernandes (@ffdiego) <br />•{" "}
+        <a href="https://github.com/ffdiego">github.com/ffdiego</a>
+      </p>
     </div>
   );
 }
