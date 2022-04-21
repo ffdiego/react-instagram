ca9964d34a8a882a63ecaccb8f82d5a929db7699
Diego <diegogkx@gmail.com>
2022-04-21 14:35:57 -0400

    fixed Avatar component default dimensions

--- src/components/Avatar.js
+++ src/components/Avatar.js
@@ -2,9 +2,10 @@ import { Link } from "react-router-dom";
 import { AvatarURL } from "../services/firestore";
 
 export default function Avatar({ user, size, clickable = true }) {
+  const _size = size || 8;
   const ImgElement = (
     <img
-      className={`rounded-full w-${size ? size : "8"} flex`}
+      className={`rounded-full w-${_size} h-${_size} flex`}
       src={AvatarURL(user)}
       onError={(e) => {
         e.target.src = "/images/avatars/default.png";
